2619b6cbb04c1625f7a5632884d9ff6b00c57fd6
xiangpan <1246033154@qq.com>
2017-11-23 12:14:54 +0800

    端口

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ from tornado.web import RequestHandler
 import urls #url配置文件
 import config #服务器相关配置文件
 
-define("port", type=int, default=8000, help="run server on the given port")
+define("port", type=int, default=80, help="run server on the given port")
 
 
 class Application(tornado.web.Application):
